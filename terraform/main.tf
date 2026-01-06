72238b3329d17b7372236e78beb39a3070005700
sky91win <sky91win@gmail.com>
2026-01-07 01:22:12 +0530

    Use default VPC and subnet data sources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,20 @@
 provider "aws" {
   region = var.aws_region
 }
+
+# Fetch default VPC automatically
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Fetch all subnets in default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
@@ -8,14 +22,14 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
 
-  vpc_id     = "vpc-xxxxxxxx"
-  subnet_ids = ["subnet-xxxx", "subnet-yyyy"]
+  vpc_id     = data.aws_vpc.default.id
+  subnet_ids = data.aws_subnets.default.ids
 
   eks_managed_node_groups = {
     prod_nodes = {
-      desired_size = 2
-      min_size     = 1
-      max_size     = 3
+      desired_size   = 2
+      min_size       = 1
+      max_size       = 3
       instance_types = [var.node_instance_type]
     }
   }
