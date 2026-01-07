1f41ac7a592d4784d9128c41a75b7f27e319a7c7
sky91win <sky91win@gmail.com>
2026-01-07 13:09:45 +0530

    Use default VPC and subnet data sources7

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,3 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-# ✅ Get only supported AZs for EKS control plane
-data "aws_subnets" "eks" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-
-  filter {
-    name   = "availability-zone"
-    values = [
-      "us-east-1a",
-      "us-east-1b",
-      "us-east-1c",
-      "us-east-1d",
-      "us-east-1f"
-    ]
-  }
-}
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
@@ -32,13 +5,11 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
 
-  # 🔴 Required for Jenkins reruns
-  create_kms_key              = false
-  cluster_encryption_config   = {}
-  create_cloudwatch_log_group = false
-
   vpc_id     = data.aws_vpc.default.id
-  subnet_ids = data.aws_subnets.eks.ids
+  subnet_ids = data.aws_subnets.default.ids
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = false
 
   eks_managed_node_groups = {
     prod_nodes = {
